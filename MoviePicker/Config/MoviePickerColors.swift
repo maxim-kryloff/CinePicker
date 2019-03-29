e80836152d19de333eaca44b41da8e9b5123ffd5
Maxim Krylov <maxim.kryloff@gmail.com>
2019-03-29 15:59:24 +0300

    Made minor style changes (#22)

--- MoviePicker/Config/MoviePickerColors.swift
+++ MoviePicker/Config/MoviePickerColors.swift
@@ -2,13 +2,13 @@ import UIKit
 
 class MoviePickerColors {
     
-    public static let green = UIColor(red: 30 / 255.0, green: 150 / 255.0, blue: 30 / 255.0, alpha: 1.0)
+    public static let green = UIColor(red: 0 / 255.0, green: 180 / 255.0, blue: 0 / 255.0, alpha: 1.0)
     
-    public static let yellow = UIColor(red: 210 / 255.0, green: 180 / 255.0, blue: 50 / 255.0, alpha: 1.0)
+    public static let yellow = UIColor(red: 255 / 255.0, green: 150 / 255.0, blue: 0 / 255.0, alpha: 1.0)
     
-    public static let red = UIColor(red: 150 / 255.0, green: 30 / 255.0, blue: 30 / 255.0, alpha: 1.0)
+    public static let red = UIColor(red: 252 / 255.0, green: 54 / 255.0, blue: 53 / 255.0, alpha: 1.0)
     
-    public static let blue = UIColor(red: 11 / 255.0, green: 120 / 255.0, blue: 254 / 255.0, alpha: 1.0)
+    public static let blue = UIColor(red: 10 / 255.0, green: 96 / 255.0, blue: 255 / 255.0, alpha: 1.0)
     
     public static let lightGray = UIColor(red: 240 / 255.0, green: 240 / 255.0, blue: 240 / 255.0, alpha: 1.0)
     
