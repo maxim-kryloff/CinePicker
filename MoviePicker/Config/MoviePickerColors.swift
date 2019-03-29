9e22924915dae74d02c039db14048488d98eb3da
Maxim Krylov <maxim.kryloff@gmail.com>
2019-03-29 22:17:10 +0300

    Made small UI improvements (#23)

--- MoviePicker/Config/MoviePickerColors.swift
+++ MoviePicker/Config/MoviePickerColors.swift
@@ -8,7 +8,7 @@ class MoviePickerColors {
     
     public static let red = UIColor(red: 252 / 255.0, green: 54 / 255.0, blue: 53 / 255.0, alpha: 1.0)
     
-    public static let blue = UIColor(red: 10 / 255.0, green: 96 / 255.0, blue: 255 / 255.0, alpha: 1.0)
+    public static let blue = UIColor(red: 0 / 255.0, green: 122 / 255.0, blue: 255 / 255.0, alpha: 1.0)
     
     public static let lightGray = UIColor(red: 240 / 255.0, green: 240 / 255.0, blue: 240 / 255.0, alpha: 1.0)
     
