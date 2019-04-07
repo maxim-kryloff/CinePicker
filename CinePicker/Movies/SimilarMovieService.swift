09446c38281cf5dac0cbe2d6fdcfce6adc3f635d
Maxim Krylov <maxim.kryloff@mail.ru>
2019-04-07 13:16:46 +0300

    Revert "Improved getting similar movies (#38)"
    This reverts commit 28fce5323bfb733abcb2c70c3cc27786d5653831.

--- CinePicker/Movies/SimilarMovieService.swift
+++ CinePicker/Movies/SimilarMovieService.swift
@@ -10,61 +10,15 @@ class SimilarMovieService {
     
     public func requestMovies(
         byMovieId movieId: Int,
-        callback: @escaping (_: [Movie], _ isLoadingDataFailed: Bool) -> Void
-    ) {
-        let concurrentDispatchQueue = DispatchQueue(label: UUID().uuidString, qos: .utility, attributes: [.concurrent])
-        let serialDispatchQueue = DispatchQueue(label: UUID().uuidString, qos: .utility)
-        
-        let dispatchGroup = DispatchGroup()
-        
-        var similarMovies: [Movie] = []
-        
-        var isRequestFailedAt: [Int: Bool] = [:]
-        
-        for page in 1...3 {
-            concurrentDispatchQueue.async(group: dispatchGroup) {
-                self.requestMovies(byMovieId: movieId, andPage: page, dispatchGroup: dispatchGroup) { (result, isRequestFailed) in
-                    defer {
-                        isRequestFailedAt[page] = isRequestFailed
-                    }
-                    
-                    if isRequestFailed {
-                        return
-                    }
-                    
-                    serialDispatchQueue.async {
-                        similarMovies += result
-                    }
-                }
-            }
-        }
-        
-        dispatchGroup.notify(queue: concurrentDispatchQueue) {
-            var isRequestFailed = false
-            
-            for page in 1...3 {
-                isRequestFailed = isRequestFailed || isRequestFailedAt[page] ?? true
-            }
-
-            callback(similarMovies, isRequestFailed)
-        }
-    }
-    
-    private func requestMovies(
-        byMovieId movieId: Int,
         andPage page: Int,
-        dispatchGroup: DispatchGroup,
         callback: @escaping (_: [Movie], _ isLoadingDataFailed: Bool) -> Void
     ) {
-        dispatchGroup.enter()
-        
         movieService.getSimilarMovies(byMovieId: movieId, andPage: page) { (result) in
             var requestResult: [Movie] = []
             var isFailed = true
             
             defer {
                 callback(requestResult, isFailed)
-                dispatchGroup.leave()
             }
             
             do {
